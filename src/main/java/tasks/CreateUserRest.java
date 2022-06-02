c949295158043d31271820616d07caf03d1971b6
juan-Ortiz10 <68971989+juan-Ortiz10@users.noreply.github.com>
2022-06-02 08:20:23 -0500

    Data Driven

--- src/main/java/tasks/CreateUserRest.java
+++ src/main/java/tasks/CreateUserRest.java
@@ -1,26 +1,28 @@
 package tasks;
 
 import interactions.ConsumeService;
-import model.ModelCreateUserRest;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 
-import java.util.List;
+import java.util.Map;
 
 public class CreateUserRest implements Task {
-    private List<ModelCreateUserRest> modelCreateUserRests;
+    private Map<String, String> request;
 
-    public CreateUserRest(List<ModelCreateUserRest> modelCreateUserRests) {
-        this.modelCreateUserRests = modelCreateUserRests;
+    public CreateUserRest(Map<String, String> request) {
+        this.request = request;
     }
 
-    public static CreateUserRest with(List<ModelCreateUserRest> modelCreateUserRests) {
-        return Tasks.instrumented(CreateUserRest.class, modelCreateUserRests);
+    public static CreateUserRest with(Map<String, String> request) {
+        return Tasks.instrumented(CreateUserRest.class, request);
     }
 
     @Override
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(ConsumeService.withPost(modelCreateUserRests.get(0).toString()));
+        actor.attemptsTo(ConsumeService.withPost("{" +
+                "\"name\":" + '\"' + request.get("name") + '\"' +
+                ", \"job\":" + '\"' + request.get("job") + '\"' +
+                '}'));
     }
 }
--- src/test/java/stepdefinitions/rest/StepDefinitionRestCreateUser.java
+++ src/test/java/stepdefinitions/rest/StepDefinitionRestCreateUser.java
@@ -1,15 +1,16 @@
 package stepdefinitions.rest;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import model.ModelCreateUserRest;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
 import questions.LastResponseStatusCode;
 import tasks.CreateUserRest;
 
 import java.util.List;
+import java.util.Map;
 
 import static net.serenitybdd.screenplay.GivenWhenThen.*;
 import static net.serenitybdd.screenplay.actors.OnStage.*;
@@ -23,8 +24,9 @@ public class StepDefinitionRestCreateUser {
         theActorCalled("Brandon").whoCan(CallAnApi.at(BASE_URL.toString()));
     }
     @When("you create an user")
-    public void youCreateAnUser(List<ModelCreateUserRest> modelCreateUserRests){
-        theActorInTheSpotlight().attemptsTo(CreateUserRest.with(modelCreateUserRests));
+    public void youCreateAnUser(DataTable data){
+        List<Map<String, String>> newlista = data.asMaps();
+        theActorInTheSpotlight().attemptsTo(CreateUserRest.with(newlista.get(0)));
     }
     @Then("I should see the user created")
     public void iShouldSeeTheUserCreated(){
